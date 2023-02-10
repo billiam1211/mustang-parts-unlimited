9bea68199210336e8a2281a45e00a14ce7f3f5d4
Bill Bakopoulos <bbakopoulos@gmail.com>
2023-02-10 15:38:23 -0600

    saving progress on featured categories

--- assets/js/theme/global.js
+++ assets/js/theme/global.js
@@ -1,5 +1,4 @@
 import 'focus-within-polyfill';
-
 import './global/jquery-migrate';
 import './common/select-option-plugin';
 import PageManager from './page-manager';
@@ -31,6 +30,7 @@ export default class Global extends PageManager {
         this.search();
         this.stickyHeaderManager();
         this.department();
+        this.featuredCategories();
     }
 
     stickyHeaderManager() {
@@ -71,48 +71,103 @@ export default class Global extends PageManager {
         });
     }
 
+    equalHeights(selector) {
+        // custom equal heights function for blog articles
+        const elms = document.querySelectorAll(selector);
+        const len = elms.length;
+        let tallest = 0;
+        // eslint-disable-next-line one-var
+        let elm,
+            elmHeight,
+            x;
+
+        for (x = 0; x < len; x++) {
+            elms[x].style.height = 'auto';
+        }
+
+        for (x = 0; x < len; x++) {
+            elm = elms[x];
+            elmHeight = elm.offsetHeight;
+            tallest = (elmHeight > tallest) ? elmHeight : tallest;
+        }
+
+        for (x = 0; x < len; x++) {
+            elms[x].style.height = tallest + 'px';
+        }
+
+        const debounce = (callback, wait) => {
+            let timeout;
+            return (...args) => {
+                const context = this;
+                clearTimeout(timeout);
+                timeout = setTimeout(() => callback.apply(context, args), wait);
+            };
+        };
+
+        const debounceResize = debounce(() => {
+            this.equalHeights(selector);
+        }, 250);
+
+        window.addEventListener('resize', debounceResize);
+
+        window.dispatchEvent(new Event('resize'));
+    }
+
+
     department() {
-        console.log('department', );
         if (document.querySelectorAll('li.subcategoryCard').length) {
-            console.log('1', );
-            // custom equal heights function for blog articles
-            const equalHeights = (selector) => {
-                const elms = document.querySelectorAll(selector);
-                const len = elms.length;
-                let tallest = 0;
-                // eslint-disable-next-line one-var
-                let elm,
-                    elmHeight,
-                    x;
-        
-                for (x = 0; x < len; x++) {
-                    elms[x].style.height = 'auto';
-                }
-        
-                for (x = 0; x < len; x++) {
-                    elm = elms[x];
-                    elmHeight = elm.offsetHeight;
-                    tallest = (elmHeight > tallest) ? elmHeight : tallest;
-                }
-        
-                for (x = 0; x < len; x++) {
-                    // eslint-disable-next-line prefer-template
-                    elms[x].style.height = tallest + 'px';
-                }
-            };
-            const debounce = (callback, wait) => {
-                let timeout;
-                return (...args) => {
-                    const context = this;
-                    clearTimeout(timeout);
-                    timeout = setTimeout(() => callback.apply(context, args), wait);
-                };
-            };
-            const debounceResize = debounce(() => {
-                equalHeights('li.subcategoryCard');
-            }, 250);
-            window.addEventListener('resize', debounceResize);
-            window.dispatchEvent(new Event('resize'));
+            this.equalHeights('li.subcategoryCard');
+        }
+    }
+
+    featuredCategories() {
+        if (document.querySelectorAll('li.featuredCategory-item').length) {
+            this.equalHeights('li.featuredCategory-item');
+            $('ul.featuredCategory-list').slick({
+                "dots": false,
+                "infinite": true,
+                "slidesToShow": 1,
+                "slidesToScroll": 1,
+                "mobileFirst": true,
+                "responsive": [
+                    {
+                        "breakpoint": 1261,
+                        "settings": {
+                            "slidesToShow": 5,
+                            "slidesToScroll": 5,
+                        }
+                    },
+                    {
+                        "breakpoint": 1024,
+                        "settings": {
+                            "slidesToShow": 4,
+                            "slidesToScroll": 4,
+                        }
+                    },
+                    {
+                        "breakpoint": 801,
+                        "settings": {
+                            "slidesToShow": 3,
+                            "slidesToScroll": 3,
+                        }
+                    },
+                    {
+                        "breakpoint": 551,
+                        "settings": {
+                            "slidesToShow": 2,
+                            "slidesToScroll": 2
+                        }
+                    },
+                    {
+                        "breakpoint": 300,
+                        "settings": {
+                            "slidesToShow": 1,
+                            "slidesToScroll": 1
+                        }
+                    }
+                ]
+            });
+            document.querySelector('h2.featuredCategory-title').classList.add('loaded');
         }
     }
 }
