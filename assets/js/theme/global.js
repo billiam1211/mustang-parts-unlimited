d706a2e9f873e839a619cb00a73a0da90ab60b4f
Bill Bakopoulos <bbakopoulos@gmail.com>
2024-01-17 06:42:25 -0600

    save

--- assets/js/theme/global.js
+++ assets/js/theme/global.js
@@ -32,6 +32,7 @@ export default class Global extends PageManager {
         this.department();
         this.featuredCategories();
         this.threeColumn();
+        this.featuredCardsCarousel();
     }
 
     stickyHeaderManager() {
@@ -41,6 +42,27 @@ export default class Global extends PageManager {
                 header.classList.add('sticky');
             } else {
                 header.classList.remove('sticky');
+            }
+        });
+
+
+        let headerHeight = header.offsetHeight;
+        let headerRegionHeight = document.querySelector('div[data-content-region=header_top--global]').offsetHeight;
+        let calculatedHeight = headerHeight - headerRegionHeight;
+        if (window.innerWidth > 1023) {
+            document.querySelector('#main-content').style.marginTop = `0px`;
+        } else {
+            document.querySelector('#main-content').style.marginTop = `${calculatedHeight}px`;
+        }
+        window.addEventListener('resize', () => {
+            console.log('==> ', window.innerWidth);
+            headerHeight = document.querySelector('header.header').offsetHeight;
+            headerRegionHeight = document.querySelector('div[data-content-region=header_top--global]').offsetHeight;
+            calculatedHeight = headerHeight - headerRegionHeight;
+            if (window.innerWidth > 1023) {
+                document.querySelector('#main-content').style.marginTop = `0px`;
+            } else {
+                document.querySelector('#main-content').style.marginTop = `${calculatedHeight}px`;
             }
         });
     }
@@ -186,6 +208,93 @@ export default class Global extends PageManager {
                     item.classList.add('is-active');
                 });
             });
+        }
+    }
+
+    featuredCardsCarousel() {
+        const featuredCardsArr = $('.featuredCards');
+        if (featuredCardsArr.length) {
+            featuredCardsArr.each((index, el) => {
+                if ($(el).hasClass('carousel')) {
+                    $(el).find('ul').slick({
+                        dots: false,
+                        infinite: true,
+                        autoplay: false,
+                        slidesToShow: 4,
+                        slidesToScroll: 4,
+                        responsive: [
+                            {
+                                breakpoint: 1261,
+                                settings: {
+                                    slidesToShow: 4,
+                                    slidesToScroll: 4
+                                }
+                            },
+                            {
+                                breakpoint: 1024,
+                                settings: {
+                                    slidesToShow: 3,
+                                    slidesToScroll: 3
+                                }
+                            },
+                            {
+                                breakpoint: 801,
+                                settings: {
+                                    slidesToShow: 2,
+                                    slidesToScroll: 2
+                                }
+                            },
+                            {
+                                breakpoint: 551,
+                                settings: {
+                                    slidesToShow: 1,
+                                    slidesToScroll: 1
+                                }
+                            }
+                        ]
+                    });
+                }
+            });
+        
+        
+            // custom equal heights function for blog articles
+            const equalHeights = (selector) => {
+                const elms = document.querySelectorAll(selector);
+                const len = elms.length;
+                let tallest = 0;
+                // eslint-disable-next-line one-var
+                let elm,
+                    elmHeight,
+                    x;
+        
+                for (x = 0; x < len; x++) {
+                    elms[x].style.height = 'auto';
+                }
+        
+                for (x = 0; x < len; x++) {
+                    elm = elms[x];
+                    elmHeight = elm.offsetHeight;
+                    tallest = (elmHeight > tallest) ? elmHeight : tallest;
+                }
+        
+                for (x = 0; x < len; x++) {
+                    // eslint-disable-next-line prefer-template
+                    elms[x].style.height = tallest + 'px';
+                }
+            };
+            const debounce = (callback, wait) => {
+                let timeout;
+                return (...args) => {
+                    const context = this;
+                    clearTimeout(timeout);
+                    timeout = setTimeout(() => callback.apply(context, args), wait);
+                };
+            };
+            const debounceResize = debounce(() => {
+                equalHeights('.featuredCards-item img');
+            }, 250);
+            window.addEventListener('resize', debounceResize);
+            window.dispatchEvent(new Event('resize'));
         }
     }
 }
